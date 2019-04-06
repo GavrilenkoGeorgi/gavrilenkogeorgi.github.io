6d8ebb29916d67fc55bf8855ca8ceeb52f76a112
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-04-06 15:42:19 +0300

    Layout improvements and bug fixes

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ window.addEventListener(`scroll`, () => {
   let scrolled = window.pageYOffset
   const heroImage = document.querySelector(`.hero-image`)
   const dividerImage = document.querySelector(`.divider-image`)
-  const heroHeader = document.querySelector(`.hero-header`)
+  const heroHeaderLink = document.querySelector(`.header-link-container`)
   const detailsHeader = document.querySelector(`.details-header`)
   // hero image
   heroImage.style.top = scrolled * .04 + `em`
   heroImage.style.opacity = 1 - scrolled * 0.0075
   // header text
-  heroHeader.style.top = scrolled * .02 + `em`
-  heroHeader.style.opacity = 1 - scrolled * 0.0075
+  heroHeaderLink.style.bottom = 5 - scrolled * .02 + `em`
+  heroHeaderLink.style.opacity = 1 - scrolled * 0.0075
   // details image
   dividerImage.style.opacity = scrolled * 0.005
   // details text
