2bf1c3a58b63370f1f674e9c1a3be77caea53ef1
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-04-08 22:10:49 +0300

    More css

--- js/index.js
+++ js/index.js
@@ -3,6 +3,32 @@ console.log(`Hi!`)
 window.addEventListener(`scroll`, () => {
   let scrolled = window.pageYOffset
   const heroImage = document.querySelector(`.hero-image`)
+  heroImage.style.top = scrolled * .03 + `em`
+  heroImage.style.opacity = 1 - scrolled * 0.0075
+  const heroHeaderLink = document.querySelector(`.header-link-animation`)
+  heroHeaderLink.style.opacity = 1 - scrolled * 0.0075
+
+  const detailsHeader = document.querySelector(`.details`)
+  detailsHeader.style.opacity = scrolled * 0.004
+  /*
+  const heroHeaderLink = document.querySelector(`.header-link-animation`)
+  let currentBottom = heroHeaderLink.style.bottom
+  console.log(`Pre current bottom is ${currentBottom}`)
+  let value = currentBottom.substr(0, currentBottom.length-2)
+
+  console.log(`Current bottom is ${value}`)
+  let result = Math.abs(value - scrolled * .04).toFixed(2)
+  console.log(`Result is ${result}`)
+  /* console.log(`Current bottom is ${currentBottom}`) */
+  /* let valueToSet = parseFloat(result).toFixed(2) */
+  /* heroHeaderLink.style.bottom = value + result + `em` */
+  /*
+  heroHeaderLink.style.top = scrolled * .04 + `em`
+  console.log(`Trying to set ${result + `em`}`)
+
+  heroHeaderLink.style.opacity = 1 - scrolled * 0.0075
+  */
+  /*
   const dividerImage = document.querySelector(`.divider-image`)
   const heroHeaderLink = document.querySelector(`.header-link-container`)
   const detailsHeader = document.querySelector(`.details-header`)
@@ -16,4 +42,5 @@ window.addEventListener(`scroll`, () => {
   dividerImage.style.opacity = scrolled * 0.005
   // details text
   detailsHeader.style.opacity = scrolled * 0.005
+  */
 })
